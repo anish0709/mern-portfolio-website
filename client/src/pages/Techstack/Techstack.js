e1d64b7b4d92808d63a0b7a2ad7d86de6ba3f38d
Anish Singhal <anish.singhal0709@gmail.com>
2024-08-08 13:05:37 +0530

    Update Techstack.js

--- client/src/pages/Techstack/Techstack.js
+++ client/src/pages/Techstack/Techstack.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./Techstack.css";
-import RubberBand from "react-reveal/RubberBand";
-import Fade from "react-reveal/Fade";
+// import "./Techstack.css";
+// import RubberBand from "react-reveal/RubberBand";
+// import Fade from "react-reveal/Fade";
+import { Bounce } from 'react-awesome-reveal';
 import { TechstackList } from "../../utils/TechstackList";
 const Techstack = () => {
   return (
     <>
       <div className="container techstack" id="techstack">
-        <RubberBand>
+        <Bounce>
           <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">
             Technologies Stack
           </h2>
@@ -16,10 +17,10 @@ const Techstack = () => {
             👉 including programming Languages, frameworks, databses, front-end
             and back-end tools, and APIs
           </p>
-        </RubberBand>
+        </Bounce>
         <div className="row">
           {TechstackList.map((tech) => (
-            <Fade left>
+            // <Fade left>
               <div key={tech._id} className="col-md-3">
                 <div className="card m-2">
                   <div className="card-content">
@@ -36,7 +37,7 @@ const Techstack = () => {
                   </div>
                 </div>
               </div>
-            </Fade>
+            // </Fade>
           ))}
         </div>
       </div>
